1f40e4a9489e839e08a1a30ce4510e73699396b6
Pranay Raycha <pr746594@dal.ca>
2023-07-09 22:49:44 -0300

    Created Folder Strucuter for FrontEnd

--- src/App.routes.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from 'react-router-dom';
-import  HomePage  from './pages/home/Home';
-
-
-const router = createBrowserRouter([
-    {path: '/', element: <HomePage />}
-  ])
-
-export default router;
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import router from './App.routes';
+import AppRoutes from './routes/Routes';
 import { RouterProvider } from 'react-router-dom';
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-       <RouterProvider router={router}></RouterProvider>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+       <RouterProvider router={AppRoutes}></RouterProvider>
+    
     </>
   )
 }
