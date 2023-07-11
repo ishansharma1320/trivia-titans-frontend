b510babccffec725032374da1edbb8c0d049212b
Ishan Sharma <ishansharma1320@gmail.com>
2023-07-11 11:27:28 -0300

    SSGP-11: Completed Forgot Password Page

--- src/components/ForgotPasswordForm/ForgotPasswordForm.tsx
+++ src/components/ForgotPasswordForm/ForgotPasswordForm.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { Stepper, Step, StepLabel, Button } from '@mui/material';
+import { Stepper, Step, StepLabel, Button, TextField,Container } from '@mui/material';
 
-const steps = ['Step 1', 'Step 2', 'Step 3']; // Add your desired steps here
+const steps = ['Enter Email', 'Add New Password' ]; // Add your desired steps here
 
 const ForgotPasswordForm = () => {
   const [activeStep, setActiveStep] = useState(0);
@@ -28,33 +28,17 @@ const ForgotPasswordForm = () => {
     switch (step) {
       case 0:
         return (
-          <input
-            type="text"
-            name="field1"
-            value={formData['field1'] || ''}
-            onChange={handleFormChange}
-            placeholder="Field 1"
-          />
+          <TextField onChange={(event)=>{
+            setFormData(formData);
+            handleFormChange(event);
+          }} value={formData['email'] || ''}  fullWidth label="Email Address" id="email" name="email" />
         );
       case 1:
         return (
-          <input
-            type="text"
-            name="field2"
-            value={formData['field2'] || ''}
-            onChange={handleFormChange}
-            placeholder="Field 2"
-          />
-        );
-      case 2:
-        return (
-          <input
-            type="text"
-            name="field3"
-            value={formData['field3'] || ''}
-            onChange={handleFormChange}
-            placeholder="Field 3"
-          />
+          <>
+          <TextField type="password" onChange={handleFormChange} value={formData['password'] || ''} sx={{mb: 2}} fullWidth label="Password" id="password" name="password" />
+          <TextField type="password" onChange={handleFormChange} value={formData['confirmPassword'] || ''} fullWidth label="Confirm Password" id="confirm password" name="confirmPassword"/>
+          </>
         );
       default:
         return null;
@@ -62,7 +46,7 @@ const ForgotPasswordForm = () => {
   };
 
   return (
-    <div>
+    <Container fixed>
       <Stepper activeStep={activeStep} alternativeLabel>
         {steps.map((label) => (
           <Step key={label}>
@@ -72,9 +56,16 @@ const ForgotPasswordForm = () => {
       </Stepper>
 
       <div>
-        <div>{renderStepContent(activeStep)}</div>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          padding: '2em 0'
+        }}>{renderStepContent(activeStep)}</div>
 
-        <div>
+        <div style={{
+          display: 'flex',
+          justifyContent: 'space-between'
+        }}>
           <Button disabled={activeStep === 0} onClick={handleBack}>
             Back
           </Button>
@@ -89,7 +80,7 @@ const ForgotPasswordForm = () => {
 
         </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
