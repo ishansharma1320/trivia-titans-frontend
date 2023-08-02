fda8996b1009b1c227942077f97c43f342a27661
Utsav12198 <singh.utsav100@gmail.com>
2023-08-02 20:09:16 -0300

    commited game dashboard

--- src/App.routes.tsx
+++ src/App.routes.tsx
@@ -1,38 +1,49 @@
-import { Navigate, createBrowserRouter } from 'react-router-dom';
-import  AuthLayout  from './layouts/AuthLayout/AuthLayout';
-import HomeLayout from './layouts/HomeLayout/HomeLayout';
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout/AuthLayout";
+import HomeLayout from "./layouts/HomeLayout/HomeLayout";
 // import AuthPage from './pages/AuthPage/AuthPage';
-import LoginForm from './components/LoginForm/LoginForm';
-import ForgotPasswordForm from './components/ForgotPasswordForm/ForgotPasswordForm';
-import RegisterForm from './components/RegisterForm/RegisterForm';
-import ProfilePage from './components/ProfilePage/ProfilePage';
-import AdminPanel from './components/AdminPanel/AdminPanel';
-import GamePanel from './components/AdminPanel/GamePanel/GamePanel';
-import QuestionsPanel from './components/AdminPanel/QuestionsPanel/QuestionsPanel';
-import QuestionsTable from './components/AdminPanel/QuestionsPanel/QuestionsTable';
-import SecurityQuestionForm from './components/LoginForm/SecurityQuestionForm/SecurityQuestionForm';
-import AllQuestionsPanel from './components/AdminPanel/QuestionsPanel/AllQuestionsPanel';
+import LoginForm from "./components/LoginForm/LoginForm";
+import ForgotPasswordForm from "./components/ForgotPasswordForm/ForgotPasswordForm";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
+import ProfilePage from "./components/ProfilePage/ProfilePage";
+import AdminPanel from "./components/AdminPanel/AdminPanel";
+import GamePanel from "./components/AdminPanel/GamePanel/GamePanel";
+import QuestionsPanel from "./components/AdminPanel/QuestionsPanel/QuestionsPanel";
+import QuestionsTable from "./components/AdminPanel/QuestionsPanel/QuestionsTable";
+import SecurityQuestionForm from "./components/LoginForm/SecurityQuestionForm/SecurityQuestionForm";
+import AllQuestionsPanel from "./components/AdminPanel/QuestionsPanel/AllQuestionsPanel";
+import GameDashboard from "./components/AdminPanel/Dashboards/gameuserdashboard";
 const router = createBrowserRouter([
-    {path: '/auth', element: <AuthLayout />,
+  {
+    path: "/auth",
+    element: <AuthLayout />,
     children: [
-        {path: 'login', element: <LoginForm />},
-        {path: 'forgotPassword', element: <ForgotPasswordForm />},
-        {path: 'register', element: <RegisterForm />},
-        {path: 'login/securityQuestion', element: <SecurityQuestionForm />}
-    ]},
-    {path: '/home', element: <HomeLayout />,
+      { path: "login", element: <LoginForm /> },
+      { path: "forgotPassword", element: <ForgotPasswordForm /> },
+      { path: "register", element: <RegisterForm /> },
+      { path: "login/securityQuestion", element: <SecurityQuestionForm /> },
+    ],
+  },
+  {
+    path: "/home",
+    element: <HomeLayout />,
     children: [
-        {path: 'profile', element: <ProfilePage />},
-        {path: 'admin', element: <AdminPanel />, children:[
-            { path: '', element: <Navigate to="games" /> }, 
-            {path: 'games', element: <GamePanel />},
-            { path: 'games/view', element: <QuestionsPanel /> },
-            {path: 'questions', element: <AllQuestionsPanel/>}
-        ]},
-        // {path: '/forgotPassword', element: <ForgotPasswordForm />},
-        // {path: '/register', element: <RegisterForm />}
-    ]}
-
-  ])
+      { path: "profile", element: <ProfilePage /> },
+      {
+        path: "admin",
+        element: <AdminPanel />,
+        children: [
+          { path: "", element: <Navigate to="games" /> },
+          { path: "games", element: <GamePanel /> },
+          { path: "games/view", element: <QuestionsPanel /> },
+          { path: "questions", element: <AllQuestionsPanel /> },
+          { path: "dashboard", element: <GameDashboard /> },
+        ],
+      },
+      // {path: '/forgotPassword', element: <ForgotPasswordForm />},
+      // {path: '/register', element: <RegisterForm />}
+    ],
+  },
+]);
 
 export default router;
